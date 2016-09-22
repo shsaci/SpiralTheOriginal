37f2d5d04254f8349dc2b451538381ec72e246a7
shsaci <008@saganbrainz.com>
2016-09-22 17:23:02 +1200

    initial commit

---
+++ SpiralTests/ProgramTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace SpiralTests
+{
+    public class ProgramTests
+    {
+        [Fact]
+        public void Working ()
+        {
+            Assert.True(true);
+        }
+    }
+}
